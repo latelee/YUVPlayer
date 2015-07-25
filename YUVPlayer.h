d9dd4178d12d10e6df5c44511bdd0aeb1008cf2c
latelee <latelee@163.com>
2015-07-25 22:19:12 +0800

    first commit.nothing to do.
    Signed-off-by: latelee <latelee@163.com>

---
+++ YUVPlayer.h
@@ -0,0 +1,32 @@
+
+// YUVPlayer.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CYUVPlayerApp:
+// 有关此类的实现，请参阅 YUVPlayer.cpp
+//
+
+class CYUVPlayerApp : public CWinApp
+{
+public:
+	CYUVPlayerApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CYUVPlayerApp theApp;
